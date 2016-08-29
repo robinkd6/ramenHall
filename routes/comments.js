b54286ad483397ff1e9c1561194492d5cbc756d5
robinkd6 <rkdcruz18@gmail.com>
2016-08-29 12:24:04 -0700

    checkcommentowner function

--- routes/comments.js
+++ routes/comments.js
@@ -44,7 +44,7 @@ router.post("/", function(req, res){
 });
 
 //comments edit route
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
 	Comment.findById(req.params.comment_id, function(err, foundComment){
 		if(err){
 			res.redirect("back");
@@ -82,6 +82,24 @@ function isLoggedIn(req, res, next)
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+		if(req.isAuthenticated()){
+			Comment.findById(req.params.comment_id, function(err, foundrComment){
+				if (err){
+					res.redirect("back");
+				} else {
+					 if(foundComment.author.id.equals(req.user._id)) {
+					 	next();
+					 } else {
+					 		res.redirect('back');
+					 }
+					}
+			});
+		} else {
+			res.redirect('back');
+		};
 }
 
 module.exports = router;
