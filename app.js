f1ab7ed5df79b882e59fc05a02869a5e5e4ff504
robinkd6 <rkdcruz18@gmail.com>
2017-02-03 16:51:32 -0800

    app listen

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ var url = process.env.DATABASEURL || "mongodb://localhost/ramen_hall";
 mongoose.connect(url);
 
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.IP || 3000, function(){
    console.log("The Server Has Started!");
 });
 
